ee76f630fc4d0d36f8d99b3345fd7c2a9ed93acc
Enrico86 <64355652+Enrico86@users.noreply.github.com>
2020-05-15 18:38:54 +0200

    Agregar archivos de proyecto.

---
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TipCalculator
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private TipModel modeloTip;
+        //Creamos un objeto de la clase TipModel para poder acceder a todas las propiedades que hemos
+        //creado
+        public MainPage()
+        {
+            InitializeComponent();
+            modeloTip = new TipModel
+            //Instanciamos nuestro objeto modeloTip en el constructor de nuestra MainPage para asegurarnos que cada vez
+            //que se abra la MainPage no nos olvidemos de hacerlo. Además de esta manera puedo digamos sugerirle los valores
+            //a introducir en las propiedades
+            {
+                PorcentajePropina = 10,
+                NumeroPersonas=2
+            };
+            //Con esto estamos instanciando nuestro modeloTip con unos valores de sus propiedades
+            this.BindingContext = modeloTip;
+            //Y por último para poder enlazar las propiedades de la interfaz grafica de nuestra MainPage con las 
+            //propiedades de la clase TipModel, tenemos que decirle que el BindingContext de esta clase (this) será
+            //el objeto modeloTip (perteneciente a la clase TipModel).
+        }
+    }
+}
---
+++ TipCalculator/TipCalculator/TipModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TipCalculator
+{
+    public class TipModel : INotifyPropertyChanged
+    {
+        public Command CalculosCommand { get; set; }
+
+        public TipModel()
+        {
+            CalculosCommand = new Command(HacerCalculos);
+        }
+
+        private void HacerCalculos()
+        {
+            TotalPropina = TotalCuenta * PorcentajePropina / 100;
+            PropinaPersona = TotalPropina / NumeroPersonas;
+            TotalPorPersona = PropinaPersona + (TotalCuenta / NumeroPersonas);
+            TotalCuentaYPropina = TotalCuenta + TotalPropina;
+        }
+
+        private float _totalCuenta;
+        public float TotalCuenta
+        {
+            get
+            {
+                if (_totalCuenta < 0)
+                { _totalCuenta = 0; }
+                return _totalCuenta;
+            }
+            set { _totalCuenta = value; }
+        }
+        private float _porcentajePropina;
+        public float PorcentajePropina
+        {
+            get
+            {
+                if (_porcentajePropina < 0)
+                { _porcentajePropina = 0; }
+                return _porcentajePropina;
+            }
+            set { _porcentajePropina = value; }
+        }
+        private int _numeroPersonas;
+        public int NumeroPersonas
+        {
+            get { return _numeroPersonas; }
+            set { _numeroPersonas = value; }
+        }
+        private float _totalPropina;
+        public float TotalPropina
+        {
+            get { return _totalPropina; }
+            set
+            {
+                _totalPropina = value;
+                OnPropertyChanged();
+            }
+        }
+        private float _propinaPersona;
+        public float PropinaPersona
+        {
+            get { return _propinaPersona; }
+            set
+            {
+                _propinaPersona = value;
+                OnPropertyChanged();
+            }
+        }
+        private float _totalPorPersona;
+        public float TotalPorPersona
+        {
+            get { return _totalPorPersona; }
+            set
+            {
+                _totalPorPersona = value;
+                OnPropertyChanged();
+            }
+        }
+        private float _totalCuentaYPropina;
+        public float TotalCuentaYPropina
+        {
+            get { return _totalCuentaYPropina; }
+            set
+            {
+                _totalCuentaYPropina = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs((propertyName)));
+
+        }
+    }
+}
